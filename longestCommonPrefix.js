ce6a0d1ed9d0834a7a80659c3f719d1f80aa0c6d
svaronc <sebastianvaron96@gmail.com>
2024-04-30 18:40:00 -0500

    initial commit for longest common prefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+//Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+
+
+
+const longestCommonPrefix = function(strs) {
+    
+};
