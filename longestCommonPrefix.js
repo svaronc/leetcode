677383488e54bd676c632d39cb864c024d5b26bf
svaronc <sebastianvaron96@gmail.com>
2024-05-10 19:41:27 -0500

    solution for longest common prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -8,5 +8,16 @@
 
 
 const longestCommonPrefix = function(strs) {
-    
+  if (strs.length === 0) return '';
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+  return prefix;
 };
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix(["dog","racecar","car"]));
+console.log(longestCommonPrefix([""]));
